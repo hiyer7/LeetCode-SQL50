c15f568ba5cb4f44ed42583cba4096f610ee3f70
hiyer7 <155455574+hiyer7@users.noreply.github.com>
2025-03-08 11:45:38 -0500

    Add files via upload
    https://leetcode.com/problems/last-person-to-fit-in-the-bus/?envType=study-plan-v2&envId=top-sql-50

---
+++ 1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,30 @@
+drop table queue;
+
+CREATE TABLE `sql50`.`queue` (
+  person_id int,
+  `p_name` varchar(40),
+	weight int,
+	turn int);
+
+insert into sql50.queue
+values
+(5,"Alice", 250, 1),
+(4, "Bob", 175, 5),
+(3,"Alex", 350, 2),
+(6,"John Cena", 400, 3),
+(1,"Winston", 500, 6),
+(2,"Marie", 200, 4);
+
+select * from queue;
+
+with running_weight_cte as(
+select p_name,
+       weight,
+       turn,
+       sum(weight) over (order by turn) as running_weight
+from queue)
+
+select distinct last_value(p_name) over () as person_name
+from running_weight_cte
+where running_weight <=1000
+
